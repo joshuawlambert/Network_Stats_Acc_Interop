964c58c244bf4a985dfe08a5f7b08861eac642f3
Jonathan Goodson <jgoodson@umd.edu>
2016-08-15 17:42:14 -0400

    Added function to parse ClinVar summary and split into phenotype group lists of variants

--- SubSetMatrix.py
+++ SubSetMatrix.py
@@ -1,5 +1,10 @@
 import pandas
+from collections import defaultdict
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import seaborn
 
 def generate_snp_acc_mapping(table_file):
     with open(table_file) as snps:
@@ -18,3 +23,14 @@ def pull_snp_variants(snp_table, snp_map):
     snps.columns = [snp_map[c] for c in snps.columns]
     snps = snps.drop('', axis=1).head()
     return snps
+
+def find_clinvar_groups(clinvar_summary, assembly='GRCh37'):
+    variants = pandas.read_table(clinvar_summary)
+    groups = defaultdict(list)
+    for i, var in variants[variants.Assembly==assembly].iterrows():
+        pheno_ids = {xr.split(':')[0]: xr.split(':')[1] for xr in var.PhenotypeIDs.split(',') if ':' in xr}
+        if 'MedGen' in pheno_ids:
+            groups[pheno_ids['MedGen']].append(var['RS# (dbSNP)'])
+        else:
+            groups[None].append(var['RS# (dbSNP)'])
+    return groups
