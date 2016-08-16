f6c367121c3065afa85ae55d1ff6c388f0f32996
Katherine Thompson <katherine.thompson@uky.edu>
2016-08-16 15:58:55 -0400

    Add files via upload

--- FSA.R
+++ FSA.R
@@ -1,8 +1,6 @@
 # Load Library
 library(rFSA)
 
-args <- commandArgs(trailingOnly = TRUE)
-filename <- commandArgs[1]
 
 # Import file
 df <- read.table(filename,sep=',',header=TRUE)
@@ -11,20 +9,23 @@ disname<-colnames(df)[dim(df)[2]]
 ## Remove columns with SNPs showing only one state
 vec<-rep(NA,dim(df)[2]-1)
 for(i in 1:(dim(df)[2]-1)){
-  vec[i]<-length(levels(df[,i]))
+  vec[i]<-length(unique(df[,i]))
 }
 
-## SNPs that cannot be included becuase they only have one category:
-newdf<-df[,-which(vec<=1)]
+## Remove SNPs that only have one genotype
+if (length(which(vec<=1))>0){ print('yes')
+  keeps<-(1:(dim(df)[2]-1))[-which(vec<=1)]
+  df<-df[,c(keeps,dim(df)[2])]
+}
 
 ## Run FSA
 fit<-glmFSA(yname=disname,
             m=2,
             numrs=10,
             interactions = TRUE,
-            save_solutions = TRUE,
+            save_solutions = FALSE,
             minmax = "min",
             criterion = int.p.val,
-            data=newdf)
+            data=df)
 
-## Results output to 'FSAsolutions.csv'
+write.table(fit$table,paste0(filename,'.output'))
