fe703b830c97b072b404066a8f10902e4667a89f
arvkevi <arvkevi@gmail.com>
2016-08-16 15:33:55 -0400

    better subset

--- SubSetMatrix.py
+++ SubSetMatrix.py
@@ -40,28 +40,29 @@ def find_clinvar_groups(clinvar_summary, assembly='GRCh37'):
     return groups
 
 
-def subset_wrap(table, groups, bad_value='No Call'):
-    """table -- is a GSE data FILE minus the HEADER
-       groups -- is a dictionary keys are group labels (phenotype for ex.), values are lists of column labels
+def subset_wrap(table, groups, dependents, bad_value='No Call'):
+    """table is a pandas DataFrame
+       groups is a dictionary keys are group labels (phenotype for ex.), values are lists of column labels
+       dependents is a list the length of the number of samples
        returns a dictionary keys(group labels): values(sub dataframes)
     """
-    df = pd.read_table(table, index_col=0)
+    df = table
+    df['dependent'] = dependents #This is the dependent variable
     df.replace(bad_value, '')
-
+    
     # call generic_subset on each group
     sub_dfs = {}
     for group, col_id_list in groups.items():
         sub_dfs[group] = generic_subset(df, col_id_list)
-
+ 
     return sub_dfs
 
 
-def generic_subset(table, group):
-    """table -- is a pandas dataframe
-       group -- is a list of col labels
-    """
-
-    subset_table = table.loc[group]
-
-    return subset_table.T  # return snps-cols, ids-rows
+def generic_subset(table, col_list):
+    """table is a pandas dataframe
+       snplist is a list of ids"""    
+    col_list.append('dependent')
+    subset_table = table[col_list]
+    
+    return subset_table #pandas dataframe
 
