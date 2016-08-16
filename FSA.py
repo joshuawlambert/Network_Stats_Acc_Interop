a51b86a0e9ef8ca8d8466dec8eaf8a845678ac31
Jonathan Goodson <jgoodson@umd.edu>
2016-08-16 17:43:36 -0400

    Fixed Rscript type

--- FSA.py
+++ FSA.py
@@ -4,6 +4,6 @@ def fsa_wrapper(sdf, key):
     tmpdir = tempfile.mkdtemp()
     fn = "%s/%s.csv" % (tmpdir, key) #absolute path
     sdf.to_csv(fn)
-    subprocess.call(['/usr/bin/RScript', 'FSA.R', '%s' % (fn)])
+    subprocess.call(['/usr/bin/Rscript', 'FSA.R', '%s' % (fn)])
     
     return pd.read_table('%s.%s' % (fn, 'output'))
