275a7fc18edf9fca4a28d9260434d644a5fe0aae
Jonathan Goodson <jgoodson@umd.edu>
2016-08-17 16:10:48 -0400

    Added ability to use alternate R scripts for FSA

--- FSA.py
+++ FSA.py
@@ -1,10 +1,10 @@
 import tempfile, subprocess
 import pandas as pd
 
-def fsa_wrapper(sdf, key):
+def fsa_wrapper(sdf, key, script='FSA.R'):
     tmpdir = tempfile.mkdtemp()
     fn = "%s/%s.csv" % (tmpdir, key) #absolute path
     sdf.to_csv(fn)
-    subprocess.call(['/usr/bin/Rscript', 'FSA.R', '%s' % (fn)])
+    subprocess.call(['/usr/bin/Rscript', script, '%s' % (fn)])
     
     return pd.read_table('%s.%s' % (fn, 'output'))
