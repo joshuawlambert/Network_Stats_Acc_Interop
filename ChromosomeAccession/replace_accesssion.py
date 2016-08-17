afba149e797a0cc85d54e9dfe086732fcd25b3f1
Ubuntu <ubuntu@ip-172-16-241-115.ec2.internal>
2016-08-17 18:59:37 +0000

    Fixed field 3 option

--- ChromosomeAccession/replace_accesssion.py
+++ ChromosomeAccession/replace_accesssion.py
@@ -5,7 +5,7 @@ chrDict = {}
 
 for line in sys.stdin:
     array = line.split("\t")
-    ls = [array[0], array[4], array[6], array[8]]
+    ls = [array[0], array[4], array[6], array[9].strip()]
     for index in range(0,len(ls)):
         chrDict[ls[index]] = ls     
 
@@ -14,7 +14,7 @@ col = int(sys.argv[1])
 rep = 0 if len(sys.argv) < 4 else int(sys.argv[3])
 
 for line in open(sys.argv[2]):
-    if(line.startswith("#")):
+    if(line.startswith("#") or line.startswith("@")):
         print line
     else:
         array = line.split("\t")
